5ea4dd6a70c8861e46f43a0166d4c702f06bba19
Muhammad Saifullah <110117071+Muhammad-Saif-ullah@users.noreply.github.com>
2024-05-23 02:14:08 +0500

    Added Front-end of Admin
    I've created all the admin screens' front designs.

---
+++ src/screens/FeeStatusScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, Button, StyleSheet} from 'react-native';
+
+const FeeStatusScreen = ({route, navigation}) => {
+  const handlePress = screen => {
+    navigation.navigate(screen, {email: route.params.email});
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="View Student Fee Status"
+          onPress={() => handlePress('ViewFeeStatusScreen')}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="Add New Fee Status"
+          onPress={() => handlePress('AddNewFeeStatusScreen')}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    marginBottom: 20,
+    width: '80%',
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  button: {
+    borderRadius: 15,
+  },
+});
+
+export default FeeStatusScreen;
