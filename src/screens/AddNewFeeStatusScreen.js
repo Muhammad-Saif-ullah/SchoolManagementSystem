78d752a812682695eaaff359cf5e364547c36f45
Muhammad Saifullah <110117071+Muhammad-Saif-ullah@users.noreply.github.com>
2024-05-26 02:55:12 +0500

    Added functionality for many admin screens
    I've added the backend for most of the admin screens. Now some testing and validatios are required for them.

--- src/screens/AddNewFeeStatusScreen.js
+++ src/screens/AddNewFeeStatusScreen.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import {View, Button, StyleSheet, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, ScrollView, Alert } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 import colors from '../styles/colors';
 import FormField from '../components/FormField';
 
-const AddNewFeeStatusScreen = () => {
+const AddNewFeeStatusScreen = ({ navigation }) => {
+  const [regNo, setRegNo] = useState('');
+  const [amountDue, setAmountDue] = useState('');
+  const [amountPaid, setAmountPaid] = useState('');
+  const [payableAmount, setPayableAmount] = useState('');
+  const [paymentDate, setPaymentDate] = useState('');
+  const [lateFees, setLateFees] = useState('');
+  const [remarks, setRemarks] = useState('');
+
+  const addNewFeeStatus = () => {
+    firestore()
+      .collection('students')
+      .doc(`std${regNo}@school.com`)
+      .update({
+        feeStatus: {
+          amountDue: amountDue,
+          amountPaid: amountPaid,
+          payableAmount: payableAmount,
+          paymentDate: paymentDate,
+          lateFees: lateFees,
+          remarks: remarks,
+        },
+      })
+      .then(() => {
+        console.log(`New Fees Status added for ${regNo}!`);
+        Alert.alert('Done', 'New Fees Status added successfully');
+      })
+      .catch((error) => {
+        console.error('Error adding new fees status : ', error);
+        Alert.alert('Error', 'An error occurred. Check console.');
+
+        return;
+      });
+
+    navigation.goBack();
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <FormField label="Reg No" />
-      <FormField label="Name" />
-      <FormField label="Amount Due" />
-      <FormField label="Amount Paid" />
-      <FormField label="Payable Amount" />
-      <FormField label="Payment Date" placeholder={'DD/MM/YYYY'} />
-      <FormField label="Late Fees" />
-      <FormField label="Remarks" />
+      <FormField label="Reg No" hook={setRegNo} />
+      <FormField label="Amount Due" hook={setAmountDue} />
+      <FormField label="Amount Paid" hook={setAmountPaid} />
+      <FormField label="Payable Amount" hook={setPayableAmount} />
+      <FormField label="Payment Date (DD/MM/YYYY)" hook={setPaymentDate} />
+      <FormField label="Late Fees" hook={setLateFees} />
+      <FormField label="Remarks" hook={setRemarks} />
       <View style={styles.buttonContainer}>
-        <Button title="Add" />
+        <Button title="Add" onPress={addNewFeeStatus} />
       </View>
     </ScrollView>
   );
