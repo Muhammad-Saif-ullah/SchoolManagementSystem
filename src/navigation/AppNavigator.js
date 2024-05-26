346455d05ac6eef9588d557f363cd931abc5ee8a
uzair1103 <uzairimran408@gmail.com>
2024-05-26 21:43:17 +0500

    Teacher Portal

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -15,6 +15,9 @@ import ResultSheetScreen from '../screens/ResultSheetScreen';
 import SyllabusScreen from '../screens/SyllabusScreen';
 import TimetablesScreen from '../screens/TimetablesScreen';
 
+import ClassesScheduleScreen from '../screens/ClassesScheduleScreen';
+
+
 import Header from '../components/Header';
 
 import WelcomeScreen from '../screens/WelcomeScreen';
@@ -25,6 +28,11 @@ import StudentPortal from '../screens/StudentPortal';
 import Teacher_Login from '../screens/Teacher_Login';
 import StudentRegistrationForm from '../screens/StudentRegistrationForm.js';
 import Teacher_Portal_Screen from '../screens/Teacher_Portal_Screen.js';
+
+
+
+
+
 // import another from '../screens/another';
 
 
@@ -68,14 +76,16 @@ const AppNavigator = () => {
         <Stack.Screen name="ResultSheetScreen" component={ResultSheetScreen} />
         <Stack.Screen name="SyllabusScreen" component={SyllabusScreen} />
         <Stack.Screen name="TimetablesScreen" component={TimetablesScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="LoginAsAdmin" component={LoginAsAdmin} />
+        {/* <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="DashboardScreen" component={DashboardScreen} /> */}
+        {/* <Stack.Screen name="AdminPortalScreen" component={AdminPortalScreen} /> */}
+        
         <Stack.Screen name="LoginAsTeacher" component={Teacher_Login} />
         <Stack.Screen name="TeacherPortalScreen" component={Teacher_Portal_Screen} />
         <Stack.Screen name="LoginAsStudent" component={LoginAsStudent} />
         <Stack.Screen name="StudentPortal" component={StudentPortal} />
         <Stack.Screen name="StudentRegistrationForm" component={StudentRegistrationForm} />
-
+        <Stack.Screen name="ClassesScheduleScreen" component={ClassesScheduleScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
